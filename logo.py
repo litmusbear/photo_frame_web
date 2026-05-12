8906da3290c239ef8510c1b15885b23971bf1e02
litmusbear <siwho3@gmail.com>
2026-05-13 05:40:06 +0900

    Add files via upload

--- logo.py
+++ logo.py
@@ -5,11 +5,13 @@ def logo(picture):
         logo_file = "logos/leica.png"
     elif "NIKON" in camera_model:
         logo_file = "logos/nikon.png"
-    elif "IPHONE" in camera_model:
-        logo_file = "logos/apple.png"
     elif "SONY" in camera_model:
         logo_file = "logos/sony.png"
     elif "CANON" in camera_model:
         logo_file = "logos/canon.png"
+    elif "IPHONE" in camera_model:
+        logo_file = "logos/apple.png"
+    elif "SAMSUNG" in camera_model:
+        logo_file = "logos/samsung.png"
 
     return logo_file
