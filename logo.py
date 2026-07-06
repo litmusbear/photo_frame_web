56e2aff75cff315b7a4c2f7bcdf2947d35ef9eaa
litmusbear <siwho3@gmail.com>
2026-07-06 18:56:00 +0900

    Update logo.py

--- logo.py
+++ logo.py
@@ -17,8 +17,8 @@ def logo(picture):
         logo_file = "logos/fujifilm.png"
 
     if not logo_file:
-                exif_str = str(image._getexif()).upper()
-                if "SONY" in exif_str or "ILCE" in exif_str or "ZV-" in exif_str:
-                    logo_file = "logos/sony.png"
+        exif_str = str(picture.exif).upper()
+        if "SONY" in exif_str or "ILCE" in exif_str or "ZV-" in exif_str:
+            logo_file = "logos/sony.png"
 
     return logo_file
